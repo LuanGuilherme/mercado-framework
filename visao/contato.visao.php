ed7f04756470357f0e94b50be878098c8568e5bc
173122X <173122X@LABDMAQ01.itp.local>
2018-10-09 11:46:26 -0300

    Atualização geral

--- visao/contato.visao.php
+++ visao/contato.visao.php
@@ -1,35 +1,17 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="estilo.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-</head>
-<body>
-	<div class="contato">
-		<h1>Fale conosco</h1><br>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7312.6077810732!2d-48.01972181563719!3d-23.593431923232547!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1530096273241" width="500" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
-		<form>
-			<br>
+<div class="contato">
+    <h1>Fale conosco</h1><br>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7312.6077810732!2d-48.01972181563719!3d-23.593431923232547!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1530096273241" width="500" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <form method="POST">
+        <br>
 
-			<label for="nome">Nome: </label>
-			<input type="text" name="nome" id="name"><br>
-			<br>
-			<label for="email">E-mail: </label>
-			<input type="email" name="email" id="email"><br>
-			<br>
-			<label for="msg">Mensagem: </label>
-			<textarea id="msg" rows="10" cols="50" name="msg"></textarea><br>
-			<input type="submit" id="botao" name="VAI" onclick="mail()">
-		</form>
-	</div>
-</body>
-</html>
-<script>
-	function mail(){
-		<?php
-		mail("luangandrade10@gmail.com, gabriel.hdda@gmail.com", "Projeto WEB", $_GET["msg"], "De: ".$_GET["email"]); 
-		?>
-	}
-</script>
+        <label for="nome">Nome: </label>
+        <input type="text" name="nome" id="name"><br>
+        <br>
+        <label for="email">E-mail: </label>
+        <input type="email" name="email" id="email"><br>
+        <br>
+        <label for="msg">Mensagem: </label>
+        <textarea id="msg" rows="10" cols="50" name="msg"></textarea><br>
+        <a href="./produto/produto/email/<?= $_POST['msg'] ?>">Enviar</a>
+    </form>
+</div>
