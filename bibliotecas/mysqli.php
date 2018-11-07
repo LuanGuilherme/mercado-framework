e9fe2f40cfc54d217c945be9b018ce4cd2ddd681
1731271 <1731271@LABCMAQ04.itp.local>
2018-11-07 15:41:39 -0200

    Dados do DB por arquivo externo

--- bibliotecas/mysqli.php
+++ bibliotecas/mysqli.php
@@ -1,7 +1,15 @@
 <?php
 
 function conn() {
-    $cnx = mysqli_connect("localhost", "root", "", "LOJA");
+    $text = fopen("C:/wamp64/www/Framework-noopMVC/bibliotecas/dados.txt", "r");
+    
+    while (!feof($text)) {
+        $dados[] = trim(fgets($text));
+    }
+    fclose($text);
+    
+    $cnx = mysqli_connect($dados[0], $dados[1], $dados[2], $dados[3]);
+    print_r($dados);
     if (!$cnx) die('Deu errado a conexao!');
     return $cnx;
 }
--- controlador/pedidoControlador.php
+++ controlador/pedidoControlador.php
@@ -1,11 +1,15 @@
 <?php
 
 require 'modelo/pedidoModelo.php';
-
+/**
+  anon
+ */
 function index(){
     exibir("pedido/finalizar");
 }
-
+/**
+  anon
+ */
 function addPedido($nomeproduto, $qtd, $preco){
     adicionarPedido($nomeproduto, $qtd, $preco);
     index();
