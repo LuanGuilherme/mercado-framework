86f3f58faf6a102421dd42d8de91147f5dd13cd8
173122X <173122X@BIBLIOMAQ08.itp.local>
2018-11-20 17:07:54 -0200

    Nova pagina do admin

--- visao/usuario/admin.visao.php
+++ visao/usuario/admin.visao.php
@@ -1,2 +1,10 @@
 <h1 id="tituProdutos">Página do Administrador</h1>
+<a href='./produto/addProduto'>Adicionar produto</a>
+<a href='./produto/updateProduto'>Alterar produto</a>
+<a href='./produto/deleteProduto'>Deletar produto</a>
+<br>
+<h2>Relatórios</h2>
+<br>
+<a href='./cliente/qtdProdutosEstoque'>Produtos e sua quantidade em estoque</a>
+<a href='./cliente/categoriaProdutos'>Produtos e sua categoria</a>
 
